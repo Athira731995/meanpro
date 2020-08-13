44bd83dea6d00ede7426855206a6e86a2d337fc7
Athira731995 <athirapajithkumar7@gmail.com>
2020-08-13 12:00:38 +0530

    t

--- src/app/testhooks/testhooks.component.ts
+++ src/app/testhooks/testhooks.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import {LoggerService} from '../logger.service'
 @Component({
   selector: 'app-testhooks',
   templateUrl: './testhooks.component.html',
   styleUrls: ['./testhooks.component.css']
 })
-export class TesthooksComponent implements OnInit {
+export class TesthooksComponent implements OnInit,OnChanges {
+  ngOnChanges(changes:SimpleChanges){
+    for (const propName in changes) {
+      const chng = changes[propName];
+      const cur  = JSON.stringify(chng.currentValue);
+      const prev = JSON.stringify(chng.previousValue);
+      // this.log.push(`${propName}: currentValue = ${cur}, previousValue = ${prev}`);
+    }
+
+  }
 
   constructor(private log:LoggerService) { }
 
